a009e73e01b5ffb598eafc773dd3bf2773121d85
Markus Hinz <markus.hinz@tum.de>
2017-12-16 17:25:19 +0100

    curl both webservers

--- website.sh
+++ website.sh
@@ -5,6 +5,10 @@ bold=`tput bold`
 normal=`tput sgr0`
 
 while true; do
+    out=`sudo python run.py --node $node --cmd "curl -s 13.0.1.1"`
+    date=`date`
+    echo $date -- $bold$out$normal
+
     out=`sudo python run.py --node $node --cmd "curl -s 11.0.4.1:8000"`
     date=`date`
     echo $date -- $bold$out$normal
