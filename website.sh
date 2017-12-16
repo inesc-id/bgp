36ff903d3427da8a9ff12a614cbdf53d30c0e81b
Markus Hinz <markus.hinz@tum.de>
2017-12-16 18:53:02 +0100

    small modifications

--- website.sh
+++ website.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 node=${1:-h1-1}
+ip=${2:-13.0.1.1}
 bold=`tput bold`
 normal=`tput sgr0`
 
 while true; do
-    out=`sudo python run.py --node $node --cmd "curl -s 13.0.1.1"`
-    date=`date`
-    echo $date -- $bold$out$normal
-
-    out=`sudo python run.py --node $node --cmd "curl -s 11.0.4.1:8000"`
+    out=`sudo python run.py --node $node --cmd "curl -s $ip"`
     date=`date`
     echo $date -- $bold$out$normal
     sleep 1
