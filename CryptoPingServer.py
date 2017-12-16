733914a7f652ae9193adc55b4638992c6c48840c
Markus Hinz <markus.hinz@tum.de>
2017-12-16 21:33:53 +0100

    no threading

--- CryptoPingServer.py
+++ CryptoPingServer.py
@@ -116,7 +116,8 @@ def main():
         conn, addr = sock.accept()
         print "New Client: " + str(addr)
 
-        threading.Thread(None,handle_connection,None,(conn,addr)).start()
+        # threading.Thread(None,handle_connection,None,(conn,addr)).start()
+        handle_connection(conn,addr)
 
     return
 
