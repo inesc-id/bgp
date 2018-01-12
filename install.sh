62d5f442d258eae424b708d560c0f3334ac39277
Markus Hinz <markus.hinz@tum.de>
2018-01-12 12:46:54 +0100

    sudo apt-get install python-setuptools

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 sudo apt-get update
 sudo apt-get install -y quagga curl screen
 sudo apt-get install python-dev
+sudo apt-get install python-setuptools
 sudo easy_install termcolor
 sudo easy_install PyCrypto
