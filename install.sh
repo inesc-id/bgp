6e87da024953f758c58c3b144a412089d1d5a8a8
Markus Hinz <markus.hinz@tum.de>
2018-01-12 13:14:02 +0100

    add mininet to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 sudo apt-get update
+cd ~
+git clone git://github.com/mininet/mininet.git
+cd mininet/util
+./install.sh -a
 sudo apt-get install -y quagga curl screen
 sudo apt-get install python-dev
 sudo apt-get install python-setuptools
