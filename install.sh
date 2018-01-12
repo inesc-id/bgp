64de2d5460c4bbc8f5752307646b2931628af72b
Markus Hinz <markus.hinz@tum.de>
2018-01-12 15:03:32 +0100

    Update readme

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ cd mininet/util
 ./install.sh -a
 sudo apt-get install -y quagga curl screen
 sudo apt-get install python-dev
-sudo apt-get install python-setuptools
+#sudo apt-get install python-setuptools
 sudo easy_install termcolor
 sudo easy_install PyCrypto
