7e820178ddadd68ee9b64173ef5ba4e07a56f135
Markus Hinz <markus.hinz@tum.de>
2018-01-12 12:44:18 +0100

    change install.sh order

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install -y quagga curl screen
-sudo easy_install termcolor
 sudo apt-get install python-dev
+sudo easy_install termcolor
 sudo easy_install PyCrypto
